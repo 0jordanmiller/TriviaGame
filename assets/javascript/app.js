059f92561d0b10a2370a3608c718ade34c330cf2
Jordan <0jordanmiller@gmail.com>
2018-05-18 15:01:19 -0400

    finished game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,12 @@
 window.onload = function() {
+
 var rightAns = 0;
 var wrongAns = 0;
-var countdown = false;
 var intervalId;
 var correctAnswer;
-var playerChose;
 var timeLeft = 30;
 var questionsPassed = 0;
-var gifsArr = ['assets/images/varg.gif', 'assets/images/crabwalk.gif', 'assets/images/gwar.gif']
+var gifsArr = ['assets/images/varg.gif', 'assets/images/windmill.gif', 'assets/images/tree-headbang.gif', 'assets/images/devil.gif', 'assets/images/crabwalk.gif', 'assets/images/gwar.gif']
 
 var question1 = {
 	q: 'What year was Metallica formed?',
@@ -25,7 +24,7 @@ var question2 = {
 	a3: 'Ozzy Osbourne',
 	a4: 'Bruce Dickinson',
 	correct: 'Ozzy Osbourne'
-}
+};
 
 var question3 = {
 	q: "What is the name of Iron Maiden's mascot?",
@@ -34,7 +33,7 @@ var question3 = {
 	a3: 'Eddie',
 	a4: 'Rob',
 	correct: 'Eddie'
-}
+};
 
 var question4 =  {
 	q: "Which band is not in the 'Big Four of Thrash'?",
@@ -43,7 +42,7 @@ var question4 =  {
 	a3: 'Anthrax',
 	a4: 'DragonForce',
 	correct: 'DragonForce'
-}
+};
 
 var questionArr = [question1, question2, question3, question4]
 
@@ -59,7 +58,7 @@ var template = `<div id="timer">
 					<li><button id="3" class ='answer'></button></li>
 					<li><button id="4" class ='answer'></button></li>
 				</ul>
-			</div>`
+			</div>`;
 
 var outOfTimeTemp = `<div id="timer">
 			Time Left: 0
@@ -69,7 +68,7 @@ var outOfTimeTemp = `<div id="timer">
 				<div id="gif">
 					
 				</div>
-			</div>`
+			</div>`;
 
 var incorrectTemp = `<div>
 				<p>You chose incorrectly</p>
@@ -77,7 +76,7 @@ var incorrectTemp = `<div>
 				<div id="gif">
 					
 				</div>
-			</div>`
+			</div>`;
 
 var correctTemp = `<div>
 				<p>You're correct</p><br>
@@ -89,18 +88,30 @@ var correctTemp = `<div>
 
 var results = `<div>
 				<p>Quiz over!</p><br>
-				<p id='right-total'></p>
-				<p id='wrong-total'></p>
-			</div>`
+				<p>answered correctly: <div id='right-total'></div></p>
+				<p>answered incorrectly: <div id='wrong-total'></div></p>
+				<button id='restart'>Restart</button>
+			</div>`;
 
-var game = $("#game")
+var game = $("#game");
 
 game.html('<button id="start">Click to start game</button>');
 
-
+$("body").on('click', '#restart', function() {
+	rightAns = 0;
+	wrongAns = 0;
+	questionsPassed = 0;
+	intervalId = setInterval(startCount, 1000)
+	game.html(template);
+	$("#question").append(question1.q);
+	$("#1").append(question1.a1);
+	$("#2").append(question1.a2);
+	$("#3").append(question1.a3);
+	$("#4").append(question1.a4);
+})
 
 document.getElementById('start').onclick = function() {
-	intervalId = setInterval(startCount, 10)
+	intervalId = setInterval(startCount, 1000)
 	game.html(template);
 	$("#question").append(question1.q);
 	$("#1").append(question1.a1);
@@ -109,10 +120,19 @@ document.getElementById('start').onclick = function() {
 	$("#4").append(question1.a4);
 } 
 
+function showResults() {
+	game.html(results);
+	$("#right-total").text(rightAns)
+	$("#wrong-total").text(wrongAns)
+}
+
 function startCount() {
 	timeLeft--;
 	$("#timer").text('Time Left: ' + timeLeft);
-	if (timeLeft === 0) {
+	if (timeLeft === 0 && questionsPassed === questionArr.length - 1) {
+		clearInterval(intervalId);
+		setTimeout(showResults, 1000 * 1);
+	} else if (timeLeft === 0) {
 		clearInterval(intervalId);		
 		$("#timer").text('Time Left:' + timeLeft)
 		game.html(outOfTimeTemp);
@@ -121,55 +141,59 @@ function startCount() {
 		$("#gif-img").attr('src', gifsArr[randomGif])
 		setTimeout(nextQuestion, 1000 * 1)
 		questionsPassed++
-		if (questionsPassed === questionArr.length) {
-			game.html(results)
-		}
 	}	
 }
 
 game.on('click', '.answer', function() {
 	if ($(this).text() === question1.correct || $(this).text() === question2.correct || $(this).text() === question3.correct || $(this).text() ===  question4.correct) {
+		rightAns++;
 		correctAnswer = $(this).text();
 		clearInterval(intervalId);
-		game.html(correctTemp);
-		rightAns++;
-		setTimeout(nextQuestion, 1000 * 1);
 		var randomGif = Math.floor(Math.random() * gifsArr.length);
-		$("#gif").html('<img id="gif-img">');
-		$("#gif-img").attr('src', gifsArr[randomGif]);
-		$("#correct-answer").append('The correct answer is ' + correctAnswer);
 		questionsPassed++;
+		if (questionsPassed === questionArr.length) {
+			game.html(correctTemp);
+			$("#gif").html('<img id="gif-img">');
+			$("#gif-img").attr('src', gifsArr[randomGif]);
+			$("#correct-answer").append('The correct answer is ' + correctAnswer);
+			setTimeout(showResults, 1000 * 2);
+		} else {
+			game.html(correctTemp);
+			$("#gif").html('<img id="gif-img">');
+			$("#gif-img").attr('src', gifsArr[randomGif]);
+			$("#correct-answer").append('The correct answer is ' + correctAnswer);
+			setTimeout(nextQuestion, 1000 * 2);
+		}
 	} else {
 		clearInterval(intervalId);
-		game.html(incorrectTemp);
 		var randomGif = Math.floor(Math.random() * gifsArr.length);
-		$("#gif").html('<img id="gif-img">');
-		$("#gif-img").attr('src', gifsArr[randomGif])
 		wrongAns++;
 		questionsPassed++;
-		setTimeout(nextQuestion, 1000 * 1)
+		if (questionsPassed === questionArr.length) {
+			game.html(incorrectTemp);
+			$("#gif").html('<img id="gif-img">');
+			$("#gif-img").attr('src', gifsArr[randomGif]);
+			$("#correct-answer").append('The correct answer is ' + questionArr[questionsPassed - 1].correct);
+			setTimeout(showResults, 1000 * 2);
+		} else {
+			game.html(incorrectTemp);
+			$("#gif").html('<img id="gif-img">');
+			$("#gif-img").attr('src', gifsArr[randomGif]);
+			$("#correct-answer").append('The correct answer is ' + questionArr[questionsPassed - 1].correct);
+			setTimeout(nextQuestion, 1000 * 2);
+		}
 	}
 })
 
 function nextQuestion() {
-	
 	timeLeft = 30;
-	intervalId = setInterval(startCount, 100)
+	intervalId = setInterval(startCount, 1000)
 	game.html(template);
 	$("#question").append(questionArr[questionsPassed].q);
 	$("#1").append(questionArr[questionsPassed].a1);
 	$("#2").append(questionArr[questionsPassed].a2);
 	$("#3").append(questionArr[questionsPassed].a3);
 	$("#4").append(questionArr[questionsPassed].a4);
-
 }
 
-
-
-
-
 }
-
-// if the html is dynamically generated, we have to create our listeners specifically as 
-// a dynamic listener, by identifying the first parent div that is used to dynamically create HTML
-// example $("#game").on('click', '.answer', function() {
